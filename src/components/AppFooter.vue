5f6e50aa3433ffc791b35ec32664dc6936274945
abra-93 <maskall93@gmail.com>
2023-09-27 19:03:50 +0300

    Add dist

--- src/components/AppFooter.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <footer class="h-20 bg-gray-100 flex justify-center items-center">
-    all rights reserved
-  </footer>
-</template>
-
-<script setup></script>
-
-<style lang="scss" scoped></style>
--- src/main.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from "vue";
-import store from "./store";
-import "./style.css";
-import App from "./App.vue";
-import router from "./routes";
-
-createApp(App).use(router).use(store).mount("#app");
